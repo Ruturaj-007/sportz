ebcb44d191b77406ba8c052c02d6dae069b35722
Ruturaj-007 <pruturaj3003@gmail.com>
2026-06-09 16:05:06 +0530

    feat: implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
+import { matchRouter } from "./routes/matches.js"
 
 const app = express();
 const PORT = 8000;
 
-// Middleware for parsing JSON
 app.use(express.json());
 
-// Root GET route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the sports API!' });
 });
 
-// Start server
+app.use('/matches', matchRouter)
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
